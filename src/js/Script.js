f8a9492bc40081176fe73626622bd816756c6c2a
nikolaenkoartem <85189081+nikolaenkoartem@users.noreply.github.com>
2021-07-22 11:59:14 +0300

    Изменил JS, но ничего там не получается, хелп

--- src/js/Script.js
+++ src/js/Script.js
@@ -28,7 +28,7 @@ calculator: {
     typeShop3 = 0,
     ];
 
-    if (((typeShopOne == 1) || (typeShopOne == 2) || (typeShopOne == 3)) && ((typeShopTwo == 1) || (typeShopTwo == 2) || (typeShopTwo == 3)) && ((typeShopThree == 1) || (typeShopThree == 2) || (typeShopThree == 3))) {
+    /*if (((typeShopOne == 1) || (typeShopOne == 2) || (typeShopOne == 3)) && ((typeShopTwo == 1) || (typeShopTwo == 2) || (typeShopTwo == 3)) && ((typeShopThree == 1) || (typeShopThree == 2) || (typeShopThree == 3))) {
 
         if (typeShopOne == 1) {
             typeShopOne = 30000;
@@ -78,10 +78,89 @@ calculator: {
     } else {
         alert("Введите число от 1 до 3!!!")
         require
-    }
+    }*/
+
+
+
+    
+    
+    
+    
+
+
+
+
+    /*var chekOne = function () {
+        if ((typeShopOne == 1) || (typeShopOne == 2) || (typeShopOne == 3)) {
+            if (typeShopOne == 1) {
+                typeShopOne = 30000;
+                typeShop1 = 7;
+            }
+
+            if (typeShopOne == 2) {
+                typeShopOne = 60000;
+                typeShop1 = 14;
+            }
+
+            if (typeShopOne == 3) {
+                typeShopOne = 80000;
+                typeShop1 = 30;
+            }
+        } else {
+            alert("Введите число от 1 до 3!!!");
+            require chekOne;
+        }
+    };
+
+    var chekTwo = function () {
+        if ((typeShopTwo == 1) || (typeShopTwo == 2) || (typeShopTwo == 3)) {
+            if (typeShopTwo == 1) {
+                typeShopTwo = 0;
+                typeShop2 = 0;
+            }
+
+            if (typeShopTwo == 2) {
+                typeShopTwo = 50000;
+                typeShop2 = 10;
+            }
+
+            if (typeShopTwo == 3) {
+                typeShopTwo = 100000;
+                typeShop2 = 30;
+            }
+        } else {
+            alert("Введите число от 1 до 3!!!");
+            require chekTwo;
+        }
+
+    };
+
+    var chekThree = function () {
+        if ((typeShopThree == 1) || (typeShopThree == 2) || (typeShopThree == 3)) {
+            if (typeShopThree == 1) {
+                typeShopThree = 0;
+                typeShop3 = 0;
+            }
+
+            if (typeShopThree == 2) {
+                typeShopThree = 10000;
+                typeShop3 = 3;
+            }
+
+            if (typeShopThree == 3) {
+                typeShopThree = 50000;
+                typeShop3 = 5;
+            }
+        } else {
+            alert("Введите число от 1 до 3!!!");
+            require chekThree;
+        }
+    };
+
 
     let result = "СТОИМОСТЬ: " + +typeShopOne + +typeShopTwo + +typeShopThree + " рублей" + "          СРОКИ: " + +typeShop1 + +typeShop2 + +typeShop3 + " дней";
 
-    alert(result);
+    alert(result);*/
+
 
 }
