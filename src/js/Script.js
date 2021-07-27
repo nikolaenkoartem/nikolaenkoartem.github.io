7189a1a980c05480e28dd89abcb3b7fa8c567cd5
nikolaenkoartem <85189081+nikolaenkoartem@users.noreply.github.com>
2021-07-27 13:30:20 +0300

    Изменения в JS и HTML

--- src/js/Script.js
+++ src/js/Script.js
@@ -1,50 +1,54 @@
 "use strict";
 
 
+/*let typeShopOne;
+let typeShopTwo;
+let typeShopThree;
 
+let typeShop1;
+let typeShop2;
+let typeShop3;
 
-let calculator;
-calculator: {
-    let typeShopOne = prompt("Выберите тип сайта (напишите цифру): 1-Сайт-визитка (30000руб.), 2-Интернет магазин (60000руб.), 3-Социальная сеть (80000руб.)");
-    let typeShopTwo = prompt("Выберите дизайн (напишите цифру): 1-Шаблонный, 2-Уникальный (+50000руб.), 3-Индивидуальный (+100000руб.)");
-    let typeShopThree = prompt("Адаптивность (напишите цифру): 1-ПК, 2-Смартфоны (+10000руб.), 3-ПК и Смартфоны (+50000руб.)");
-    let mainList = [];
-    mainList.push(typeShopOne, typeShopTwo, typeShopThree);
-    alert(mainList);
+let calculator = {
+    typeShopOne: 0,
+    typeShopTwo: 0,
+    typeShopThree: 0,
+};
 
-    let calc = [
-    typeShopOne,
-    typeShopTwo,
-    typeShopThree,
-    ];
-
-    var typeShop1;
-    var typeShop2;
-    var typeShop3;
-
-    let calc2 = [
+let calc = [
     typeShop1 = 0,
     typeShop2 = 0,
     typeShop3 = 0,
     ];
 
-    /*if (((typeShopOne == 1) || (typeShopOne == 2) || (typeShopOne == 3)) && ((typeShopTwo == 1) || (typeShopTwo == 2) || (typeShopTwo == 3)) && ((typeShopThree == 1) || (typeShopThree == 2) || (typeShopThree == 3))) {
-
+function chekOne() {
+    typeShopOne = prompt("Выберите тип сайта (напишите цифру): 1-Сайт-визитка (30000руб.), 2-Интернет магазин (60000руб.), 3-Социальная сеть (80000руб.)");
+    if ((typeShopOne == 1) || (typeShopOne == 2) || (typeShopOne == 3)) {
         if (typeShopOne == 1) {
             typeShopOne = 30000;
-            typeShop1 = 7;
+            typeShop1 = 10;
         }
 
         if (typeShopOne == 2) {
             typeShopOne = 60000;
-            typeShop1 = 14;
+            typeShop1 = 20;
         }
 
         if (typeShopOne == 3) {
             typeShopOne = 80000;
-            typeShop1 = 30;
+            typeShop1 = 40;
         }
 
+    } else {
+        alert("Введите число от 1 до 3!!!");
+        chekOne();
+    }
+};
+chekOne();
+
+function chekTwo() {
+    typeShopTwo = prompt("Выберите дизайн (напишите цифру): 1-Шаблонный, 2-Уникальный (+50000руб.), 3-Индивидуальный (+100000руб.)");
+    if ((typeShopTwo == 1) || (typeShopTwo == 2) || (typeShopTwo == 3)) {
         if (typeShopTwo == 1) {
             typeShopTwo = 0;
             typeShop2 = 0;
@@ -57,9 +61,18 @@ calculator: {
 
         if (typeShopTwo == 3) {
             typeShopTwo = 100000;
-            typeShop2 = 30;
+            typeShop2 = 20;
         }
+    } else {
+        alert("Введите число от 1 до 3!!!");
+        chekTwo();
+    }
+};
+chekTwo();
 
+function chekThree() {
+    typeShopThree = prompt("Адаптивность (напишите цифру): 1-ПК, 2-Смартфоны (+10000руб.), 3-ПК и Смартфоны (+50000руб.)");
+    if ((typeShopThree == 1) || (typeShopThree == 2) || (typeShopThree == 3)) {
         if (typeShopThree == 1) {
             typeShopThree = 0;
             typeShop3 = 0;
@@ -74,93 +87,40 @@ calculator: {
             typeShopThree = 50000;
             typeShop3 = 5;
         }
-
     } else {
-        alert("Введите число от 1 до 3!!!")
-        require
-    }*/
+        alert("Введите число от 1 до 3!!!");
+        chekThree();
+    }
+};
+chekThree();
 
+let result1 = typeShopOne + typeShopTwo + typeShopThree;
+let result2 = typeShop1 + typeShop2 + typeShop3;
+let result = "СТОИМОСТЬ: " + result1 + " рублей" + "          СРОКИ: " + result2 + " дней";
 
+alert(result);
 
-    
-    
-    
-    
+console.log(result1);
+console.log(result2);
+console.log(result);*/
 
+$('a[href^="#"]').click(function () {
+    let valHref = $(this).attr("href");
+    $('html, body').animate({
+        scrollTop: $(valHref).offset().top - 60 + "px"
+    });
+});
 
-
-
-    /*var chekOne = function () {
-        if ((typeShopOne == 1) || (typeShopOne == 2) || (typeShopOne == 3)) {
-            if (typeShopOne == 1) {
-                typeShopOne = 30000;
-                typeShop1 = 7;
-            }
-
-            if (typeShopOne == 2) {
-                typeShopOne = 60000;
-                typeShop1 = 14;
-            }
-
-            if (typeShopOne == 3) {
-                typeShopOne = 80000;
-                typeShop1 = 30;
-            }
-        } else {
-            alert("Введите число от 1 до 3!!!");
-            require chekOne;
+$(window).scroll(() => {
+    let scrollDistance = $(window).scrollTop();
+    $(".section").each((i, el) => {
+        if ($(el).offset().top - $("nav").outerHeight() <= scrollDistance) {
+            $("nav a").each((i, el) => {
+                if ($(el).hasClass("active")) {
+                    $(el).removeClass("active");
+                }
+            });
+            $('nav li: eq(' + i + ')').find('a').addClass('active');
         }
-    };
-
-    var chekTwo = function () {
-        if ((typeShopTwo == 1) || (typeShopTwo == 2) || (typeShopTwo == 3)) {
-            if (typeShopTwo == 1) {
-                typeShopTwo = 0;
-                typeShop2 = 0;
-            }
-
-            if (typeShopTwo == 2) {
-                typeShopTwo = 50000;
-                typeShop2 = 10;
-            }
-
-            if (typeShopTwo == 3) {
-                typeShopTwo = 100000;
-                typeShop2 = 30;
-            }
-        } else {
-            alert("Введите число от 1 до 3!!!");
-            require chekTwo;
-        }
-
-    };
-
-    var chekThree = function () {
-        if ((typeShopThree == 1) || (typeShopThree == 2) || (typeShopThree == 3)) {
-            if (typeShopThree == 1) {
-                typeShopThree = 0;
-                typeShop3 = 0;
-            }
-
-            if (typeShopThree == 2) {
-                typeShopThree = 10000;
-                typeShop3 = 3;
-            }
-
-            if (typeShopThree == 3) {
-                typeShopThree = 50000;
-                typeShop3 = 5;
-            }
-        } else {
-            alert("Введите число от 1 до 3!!!");
-            require chekThree;
-        }
-    };
-
-
-    let result = "СТОИМОСТЬ: " + +typeShopOne + +typeShopTwo + +typeShopThree + " рублей" + "          СРОКИ: " + +typeShop1 + +typeShop2 + +typeShop3 + " дней";
-
-    alert(result);*/
-
-
-}
+    });
+});
